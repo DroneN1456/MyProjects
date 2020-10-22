92b8e10aca1c6b406751de3ead25793b4510cb69
DroneN1456 <gugagrilo07@gmail.com>
2020-10-22 18:08:07 -0300

    Novo projetinho resolvido

---
+++ WokerSys/Entities/Worker.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace WokerSys.Entities
+{
+    class Worker
+    {
+        List<HourContract> Contracts = new List<HourContract>();
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+
+        public Worker (string name, string level, double baseSalary)
+        {
+            Name = name;
+            Level = Enum.Parse<WorkerLevel>(level);
+            BaseSalary = baseSalary;
+        }
+
+        public void AddContract(HourContract contract)
+        {
+            Contracts.Add(contract);
+        }
+        public void RemoveContract(HourContract contract)
+        {
+            Contracts.Remove(contract);
+        }
+
+        public double income(DateTime date)
+        {
+            DateTime searchDate = date;
+            double valorTotal = 0;
+            foreach(HourContract contrato in Contracts)
+            {
+                if (contrato.Date.Year == searchDate.Year && contrato.Date.Month == searchDate.Month)
+                {
+                    valorTotal += contrato.totalValue();
+                }
+            }
+            return valorTotal;
+        
+        }
+    }
+}
---
+++ WokerSys/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Dynamic;
+using WokerSys.Entities;
+
+namespace WokerSys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Department's name : ");
+            string depName = Console.ReadLine();
+            Department department = new Department(depName);
+            Console.WriteLine("Enter worker data:");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Level(Junir/MidLevel/Senior): ");
+            string level = Console.ReadLine();
+            Console.Write("Base salary: ");
+            double baseSalary = double.Parse(Console.ReadLine());
+            Worker worker = new Worker(name, level, baseSalary);
+            Console.Write("How many contracts to this worker? ");
+            int N = int.Parse(Console.ReadLine());
+            for(int i = 0; i < N; i++)
+            {
+                Console.WriteLine("Enter " + (i + 1) + "# contract data:");
+                Console.Write("Date(DD/MM/YYYY): ");
+                string date = Console.ReadLine();
+                DateTime dateTime = DateTime.Parse(date);
+                Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine());
+                Console.Write("Hours: ");
+                int hour = int.Parse(Console.ReadLine());
+                HourContract contract = new HourContract(dateTime, valuePerHour, hour);
+                worker.AddContract(contract);
+            }
+            Console.WriteLine("");
+            Console.Write("Enter month and year to calculate income(MM/YYYY): ");
+            DateTime date2 = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Name: " + worker.Name);
+            Console.WriteLine("Department: " + department.Name);
+            double income = worker.income(date2);
+            Console.WriteLine("Income for " + date2.ToString("MM/yyyy") + ":" + " " + income);
+        }
+    }
+}
