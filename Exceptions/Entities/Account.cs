9e9d4dbcb22a0830f25cc3262667ecf9f66f8558
DroneN1456 <gugagrilo07@gmail.com>
2020-10-25 18:46:02 -0300

    Mais um exercicio resolvido fml :)

---
+++ Exceptions/Entities/Account.cs
@@ -0,0 +1,45 @@
+﻿using Exceptions.Entities.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Exceptions.Entities
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithdrawLimit { get; set; }
+
+        public Account(int number, string holder, double balance, double withdrawlimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithdrawLimit = withdrawlimit;
+        }
+        public void Deposit(double value)
+        {
+            Balance += value;
+        }
+
+        public void Withdraw(double value)
+        {
+            if (value > WithdrawLimit)
+            {
+                throw new DomainException("Valor do saque não pode ser maior que o limite da conta"!);
+            }
+
+            if (value > Balance)
+            {
+                throw new DomainException("Valor do saque não pode ser maior que o valor do saldo da conta!");
+            }
+
+            Balance -= value;
+             
+        }
+
+    }
+}
---
+++ Exceptions/Program.cs
@@ -0,0 +1,36 @@
+﻿using Exceptions.Entities;
+using Exceptions.Entities.Exceptions;
+using System;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Console.WriteLine("Enter account data");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+                Console.Write("Initial balance:");
+                double balance = double.Parse(Console.ReadLine());
+                Console.Write("Withdraw limit: ");
+                double withdrawlimit = double.Parse(Console.ReadLine());
+                Account acc = new Account(number, holder, balance, withdrawlimit);
+                Console.WriteLine();
+                Console.Write("Enter amount for withdraw: ");
+                acc.Withdraw(double.Parse(Console.ReadLine()));
+                Console.WriteLine("New balance: " + acc.Balance.ToString("F2"));
+            }
+
+            catch(DomainException e)
+            {
+                Console.WriteLine("Erro! " + e.Message);
+            }
+        }
+    }
+}
