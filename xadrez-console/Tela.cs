ad707b58ddaff09d81b04cde1cff9fcb0c5c4a29
DroneN1456 <gugagrilo07@gmail.com>
2020-10-27 12:26:23 -0300

    Colocando cor nas peça :)

--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.NetworkInformation;
 using System.Text;
 using tabuleiro;
 
@@ -11,6 +12,7 @@ namespace xadrez_console
         {
             for (int i = 0; i < tab.linhas; i++)
             {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < tab.colunas; j++)
                 {
                     if (tab.peca(i, j) == null)
@@ -19,7 +21,8 @@ namespace xadrez_console
                     }
                     else
                     {
-                        Console.Write(tab.peca(i, j) + " " );
+                        imprimirPeca(tab.peca(i, j));
+                        Console.Write(" ");
                     }
                   
                    
@@ -29,6 +32,24 @@ namespace xadrez_console
 
                 
             }
+
+            Console.WriteLine("  a b c d e f g h");
+        }
+
+        public static void imprimirPeca(Peca peca)
+        {
+            if (peca.cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
+            }
+
         }
     }
 }
